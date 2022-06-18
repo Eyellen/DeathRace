f6ac5d1a81331b7dad8b0fae60c51055ea18e457
Eyellen <83500523+Eyellen@users.noreply.github.com>
2022-06-18 18:09:20 +0300

    Revert "Merge branch 'main' into Art"
    This reverts commit 23ba3a13d13bfe4ca14b9a5d6a2679dafe6c6217, reversing
    changes made to c53d886935d09ed7881cedda7aaf255575898a2e.

--- Assets/Resources/Scripts/Car/DestroyedCar.cs
+++ Assets/Resources/Scripts/Car/DestroyedCar.cs
@@ -6,6 +6,10 @@ using Mirror;
 public class DestroyedCar : NetworkBehaviour
 {
     private Rigidbody _rigidbody;
+    private CarBackPlateDamageable _backPlateDamageable;
+
+    [SyncVar(hook = nameof(DestroyBackPlate))]
+    private bool _isBackPlateBroken;
 
     [SerializeField] private float _explosionForce;
 
@@ -17,6 +21,7 @@ public class DestroyedCar : NetworkBehaviour
     private void Awake()
     {
         _rigidbody = GetComponent<Rigidbody>();
+        _backPlateDamageable = GetComponent<CarBackPlateDamageable>();
     }
 
     void Start()
@@ -24,7 +29,10 @@ public class DestroyedCar : NetworkBehaviour
         if (isServer)
         {
             Explode();
+            InitializeBackPlate();
         }
+
+        CheckIfBackPlateBroken();
     }
 
     private void Explode()
@@ -33,5 +41,45 @@ public class DestroyedCar : NetworkBehaviour
         int z = Random.Range(-1, 1);
         Vector3 impactPoint = transform.position + new Vector3(x, 0, z);
         _rigidbody.AddForceAtPosition(Vector3.up * _explosionForce, impactPoint, ForceMode.VelocityChange);
+    }
+
+    private void InitializeBackPlate()
+    {
+#if UNITY_EDITOR
+        if (!Car)
+        {
+            Debug.LogError("Car is not set for DestroyedCar. Can't initialize BackPlate.");
+            return;
+        }
+        if (!Car.GetComponent<CarBackPlateDamageable>())
+        {
+            Debug.LogError($"Car wasn't set correct or Car doesn't have {nameof(CarBackPlateDamageable)} script. Can't initialize BackPlate");
+            return;
+        }
+#endif
+        _backPlateDamageable.Initialize(Car.GetComponent<CarBackPlateDamageable>());
+    }
+
+    private void CheckIfBackPlateBroken()
+    {
+        if(_backPlateDamageable.IsBroken)
+        {
+            CmdSetIsBackPlateBroken(true);
+        }
+    }
+
+    [Command(requiresAuthority = false)]
+    private void CmdSetIsBackPlateBroken(bool isBackPlateBroken)
+    {
+        _isBackPlateBroken = isBackPlateBroken;
+    }
+
+    private void DestroyBackPlate(bool oldValue, bool newValue)
+    {
+        GameObject currentBackPlate = transform.Find("Body/BackPlate")?.gameObject;
+
+        if (!currentBackPlate) return;
+
+        Destroy(currentBackPlate);
     }
 }
