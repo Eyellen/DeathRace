1dc3f33e1cbf03a1914b3e45732bc8583311c1f7
Eyellen <83500523+Eyellen@users.noreply.github.com>
2022-06-18 22:25:00 +0300

    Added Debug Log to Escape button in play mode

--- Assets/Resources/Scripts/Debug/ConsoleToGUI.cs
+++ Assets/Resources/Scripts/Debug/ConsoleToGUI.cs
@@ -4,6 +4,8 @@ namespace DebugStuff
 {
     public class ConsoleToGUI : MonoBehaviour
     {
+        [SerializeField] private bool _isActive;
+
         //#if !UNITY_EDITOR
         static string myLog = "";
         private string output;
@@ -19,6 +21,14 @@ namespace DebugStuff
             Application.logMessageReceived -= Log;
         }
 
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                _isActive = !_isActive;
+            }
+        }
+
         public void Log(string logString, string stackTrace, LogType type)
         {
             output = $"[{type}] " + logString;
@@ -32,6 +42,8 @@ namespace DebugStuff
 
         void OnGUI()
         {
+            if (!_isActive) return;
+
             //if (!Application.isEditor) //Do not display in editor ( or you can use the UNITY_EDITOR macro to also disable the rest)
             {
                 myLog = GUI.TextArea(new Rect(10, Screen.height / 2 - 10, Screen.width - 10, Screen.height / 2), myLog);
