d07b7daca995987a5da7c076f4d0ff803092c45f
Eyellen <83500523+Eyellen@users.noreply.github.com>
2022-06-18 12:35:57 +0300

    Deleted BackPlate part from DestroyedCar

--- Assets/Resources/Scripts/Car/DestroyedCar.cs
+++ Assets/Resources/Scripts/Car/DestroyedCar.cs
@@ -6,10 +6,6 @@ using Mirror;
 public class DestroyedCar : NetworkBehaviour
 {
     private Rigidbody _rigidbody;
-    private CarBackPlateDamageable _backPlateDamageable;
-
-    [SyncVar(hook = nameof(DestroyBackPlate))]
-    private bool _isBackPlateBroken;
 
     [SerializeField] private float _explosionForce;
 
@@ -21,7 +17,6 @@ public class DestroyedCar : NetworkBehaviour
     private void Awake()
     {
         _rigidbody = GetComponent<Rigidbody>();
-        _backPlateDamageable = GetComponent<CarBackPlateDamageable>();
     }
 
     void Start()
@@ -29,10 +24,7 @@ public class DestroyedCar : NetworkBehaviour
         if (isServer)
         {
             Explode();
-            InitializeBackPlate();
         }
-
-        CheckIfBackPlateBroken();
     }
 
     private void Explode()
@@ -41,45 +33,5 @@ public class DestroyedCar : NetworkBehaviour
         int z = Random.Range(-1, 1);
         Vector3 impactPoint = transform.position + new Vector3(x, 0, z);
         _rigidbody.AddForceAtPosition(Vector3.up * _explosionForce, impactPoint, ForceMode.VelocityChange);
-    }
-
-    private void InitializeBackPlate()
-    {
-#if UNITY_EDITOR
-        if (!Car)
-        {
-            Debug.LogError("Car is not set for DestroyedCar. Can't initialize BackPlate.");
-            return;
-        }
-        if (!Car.GetComponent<CarBackPlateDamageable>())
-        {
-            Debug.LogError($"Car wasn't set correct or Car doesn't have {nameof(CarBackPlateDamageable)} script. Can't initialize BackPlate");
-            return;
-        }
-#endif
-        _backPlateDamageable.Initialize(Car.GetComponent<CarBackPlateDamageable>());
-    }
-
-    private void CheckIfBackPlateBroken()
-    {
-        if(_backPlateDamageable.IsBroken)
-        {
-            CmdSetIsBackPlateBroken(true);
-        }
-    }
-
-    [Command(requiresAuthority = false)]
-    private void CmdSetIsBackPlateBroken(bool isBackPlateBroken)
-    {
-        _isBackPlateBroken = isBackPlateBroken;
-    }
-
-    private void DestroyBackPlate(bool oldValue, bool newValue)
-    {
-        GameObject currentBackPlate = transform.Find("Body/BackPlate")?.gameObject;
-
-        if (!currentBackPlate) return;
-
-        Destroy(currentBackPlate);
     }
 }
