480712bd8ac6efa630f66745774145b4f06b6689
Eyellen <83500523+Eyellen@users.noreply.github.com>
2022-06-18 09:41:17 +0300

    Update CarEffects.cs

--- Assets/Resources/Scripts/Car/CarEffects.cs
+++ Assets/Resources/Scripts/Car/CarEffects.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class CarEffects : MonoBehaviour
 {
-    [SerializeField] private Minigun _minigun;
+    [SerializeField] private GunBase _gun;
 
     [Header("Bullet cartridge particles")]
     [SerializeField] private ParticleSystem _bulletCartridgeParticles;
@@ -17,12 +17,12 @@ public class CarEffects : MonoBehaviour
 
     private void BulletCartridgeEffect()
     {
-        if(_minigun.IsShooting && !_isBulletCartridgeParticlesPlaying)
+        if(_gun.IsShooting && !_isBulletCartridgeParticlesPlaying)
         {
             _isBulletCartridgeParticlesPlaying = true;
             _bulletCartridgeParticles.Play();
         }
-        if (!_minigun.IsShooting && _isBulletCartridgeParticlesPlaying)
+        if (!_gun.IsShooting && _isBulletCartridgeParticlesPlaying)
         {
             _isBulletCartridgeParticlesPlaying = false;
             _bulletCartridgeParticles.Stop();
