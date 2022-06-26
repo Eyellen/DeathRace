795947d72eee08cc6e5f6b7d370fbc801f920c5a
Eyellen <83500523+Eyellen@users.noreply.github.com>
2022-06-26 19:02:52 +0300

    Added sensitivity options for User Settings

--- Assets/Resources/Scripts/UI/Settings/SettingsUser.cs
+++ Assets/Resources/Scripts/UI/Settings/SettingsUser.cs
@@ -1,13 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SettingsUser : MonoBehaviour
 {
     private string _username;
 
+    [SerializeField] private GameObject _sensitivityField;
+    [SerializeField] private GameObject _xSensitivityField;
+    [SerializeField] private GameObject _ySensitivityField;
+
     public void SetUsername(string username)
     {
         _username = username;
+    }
+
+    public void ToggleSplitSensitivity(bool isSplit)
+    {
+        _sensitivityField.SetActive(!isSplit);
+        _xSensitivityField.SetActive(isSplit);
+        _ySensitivityField.SetActive(isSplit);
     }
 }
