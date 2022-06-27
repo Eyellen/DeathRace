6373d2bac4d7106eb2342cd584f6a33c116af286
Eyellen <83500523+Eyellen@users.noreply.github.com>
2022-06-27 06:18:36 +0300

    Update SettingsAudio.cs
    - Added properties

--- Assets/Resources/Scripts/UI/Settings/SettingsAudio.cs
+++ Assets/Resources/Scripts/UI/Settings/SettingsAudio.cs
@@ -9,18 +9,25 @@ public class SettingsAudio : MonoBehaviour
     [SerializeField] private AudioMixer _gameSoundsMixer;
     [SerializeField] private AudioMixer _ambienceMixer;
 
+    public float GeneralVolume { get; private set; }
+    public float GameSoundsVolume { get; private set; }
+    public float AmbienceVolume { get; private set; }
+
     public void SetGeneralVolume(float volume)
     {
+        GeneralVolume = volume;
         _generalMixer.SetFloat("Volume", FloatToDecibel(volume));
     }
 
     public void SetGameSoundsVolume(float volume)
     {
+        GameSoundsVolume = volume;
         _gameSoundsMixer.SetFloat("Volume", FloatToDecibel(volume));
     }
 
     public void SetAmbienceVolume(float volume)
     {
+        AmbienceVolume = volume;
         _ambienceMixer.SetFloat("Volume", FloatToDecibel(volume));
     }
 
