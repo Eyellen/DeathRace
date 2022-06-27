340fae95a0f0cb9c843da71e1ca603511096cec9
Eyellen <83500523+Eyellen@users.noreply.github.com>
2022-06-27 06:18:05 +0300

    Update SettingsUser.cs
    - Added Sensitivity settings to UserSettings
    - Added properties

--- Assets/Resources/Scripts/UI/Settings/SettingsUser.cs
+++ Assets/Resources/Scripts/UI/Settings/SettingsUser.cs
@@ -7,9 +7,14 @@ public class SettingsUser : MonoBehaviour
 {
     private string _username;
 
+    private Vector2 _sensitivity;
+
     [SerializeField] private GameObject _sensitivityField;
     [SerializeField] private GameObject _xSensitivityField;
     [SerializeField] private GameObject _ySensitivityField;
+
+    public string Username { get => _username; }
+    public Vector2 Sensitivity { get => _sensitivity; }
 
     public void SetUsername(string username)
     {
@@ -21,5 +26,25 @@ public class SettingsUser : MonoBehaviour
         _sensitivityField.SetActive(!isSplit);
         _xSensitivityField.SetActive(isSplit);
         _ySensitivityField.SetActive(isSplit);
+
+        if(!isSplit)
+        {
+            _sensitivity = new Vector2(_sensitivity.x, _sensitivity.x);
+        }
+    }
+
+    public void SetSensitivity(float value)
+    {
+        _sensitivity.x = _sensitivity.y = value;
+    }
+
+    public void SetXSensitivity(float value)
+    {
+        _sensitivity.x = value;
+    }
+
+    public void SetYSensitivity(float value)
+    {
+        _sensitivity.y = value;
     }
 }
