70d7adb2295a3a42dd80ca21423362555b8890ff
Eyellen <83500523+Eyellen@users.noreply.github.com>
2022-06-28 21:11:13 +0300

    Finished Save/Loading System
    In plans to rewrtie it in better way, because right now the code is terrible

--- Assets/Resources/Scripts/Data/SettingsData/SettingsLoadManager.cs
+++ Assets/Resources/Scripts/Data/SettingsData/SettingsLoadManager.cs
@@ -1,12 +1,51 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 [DisallowMultipleComponent]
 public class SettingsLoadManager : MonoBehaviour
 {
+    private SettingsGeneralData _loadedSave;
+
+    [SerializeField] private AudioMixer _generalMixer;
+    [SerializeField] private AudioMixer _gameSoundsMixer;
+    [SerializeField] private AudioMixer _ambienceMixer;
+
     private void Start()
     {
-        SettingsSaveSystem.Load();
+        _loadedSave = SettingsSaveSystem.Load();
+
+        ApplyLoadedSettings();
+    }
+
+    private void ApplyLoadedSettings()
+    {
+        Screen.SetResolution(_loadedSave.graphicsData.ResolutionWidth, _loadedSave.graphicsData.ResolutionHeight, Screen.fullScreenMode);
+        FullScreenMode[] modes = new FullScreenMode[3]
+        {
+            FullScreenMode.FullScreenWindow,
+            FullScreenMode.MaximizedWindow,
+            FullScreenMode.Windowed,
+        };
+        Screen.fullScreenMode = modes[_loadedSave.graphicsData.DisplayModeIndex];
+        QualitySettings.SetQualityLevel(_loadedSave.graphicsData.QualityIndex);
+        if(_loadedSave.graphicsData.IsShadowsEnabled)
+        {
+            QualitySettings.shadows = ShadowQuality.All;
+        }
+        else
+        {
+            QualitySettings.shadows = ShadowQuality.Disable;
+        }
+
+        _generalMixer.SetFloat("Volume", Float01ToDecibel(_loadedSave.audioData.GeneralVolume));
+        _gameSoundsMixer.SetFloat("Volume", Float01ToDecibel(_loadedSave.audioData.GameSoundsVolume));
+        _ambienceMixer.SetFloat("Volume", Float01ToDecibel(_loadedSave.audioData.AmbienceVolume));
+    }
+
+    private float Float01ToDecibel(float value)
+    {
+        return Mathf.Lerp(-80, 20, value);
     }
 }
