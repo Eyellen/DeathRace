5bcab01127dd08caa66dd346b926b9a82d9eeef2
Eyellen <83500523+Eyellen@users.noreply.github.com>
2022-06-28 06:10:08 +0300

    Added RefreshOptions to SettingsUser

--- Assets/Resources/Scripts/UI/Settings/SettingsUser.cs
+++ Assets/Resources/Scripts/UI/Settings/SettingsUser.cs
@@ -11,6 +11,7 @@ public class SettingsUser : MonoBehaviour
     [SerializeField] private TMP_InputField _usernameField;
 
     [Header("Sensitivity settings")]
+    private bool _isSensitivitySplitted;
     [SerializeField] private Toggle _splitSensitivityToggle;
     private Vector2 _sensitivity;
     [SerializeField] private GameObject _sensitivityField;
@@ -18,16 +19,19 @@ public class SettingsUser : MonoBehaviour
     [SerializeField] private GameObject _ySensitivityField;
 
     public string Username { get => _username; }
-    public bool IsSensitivitySplitted { get => _splitSensitivityToggle.isOn; }
+    public bool IsSensitivitySplitted { get => _isSensitivitySplitted; }
     public Vector2 Sensitivity { get => _sensitivity; }
 
     public void SetUsername(string username)
     {
         _username = username;
+        RefreshOptions();
     }
 
     public void ToggleSplitSensitivity(bool isSplit)
     {
+        _isSensitivitySplitted = isSplit;
+
         _sensitivityField.SetActive(!isSplit);
         _xSensitivityField.SetActive(isSplit);
         _ySensitivityField.SetActive(isSplit);
@@ -36,20 +40,41 @@ public class SettingsUser : MonoBehaviour
         {
             _sensitivity = new Vector2(_sensitivity.x, _sensitivity.x);
         }
+        RefreshOptions();
     }
 
     public void SetSensitivity(float value)
     {
         _sensitivity.x = _sensitivity.y = value;
+        RefreshOptions();
     }
 
     public void SetXSensitivity(float value)
     {
         _sensitivity.x = value;
+        RefreshOptions();
     }
 
     public void SetYSensitivity(float value)
     {
         _sensitivity.y = value;
+        RefreshOptions();
+    }
+
+    private void RefreshOptions()
+    {
+        _usernameField.text = _username;
+
+        _splitSensitivityToggle.isOn = _isSensitivitySplitted;
+
+        if(_isSensitivitySplitted)
+        {
+            _xSensitivityField.GetComponentInChildren<Slider>().value = _sensitivity.x;
+            _ySensitivityField.GetComponentInChildren<Slider>().value = _sensitivity.y;
+        }
+        else
+        {
+            _sensitivityField.GetComponentInChildren<Slider>().value = _sensitivity.x;
+        }
     }
 }
