6bf53aff143e5fd2d3f51387cdf802e92766984e
Eyellen <83500523+Eyellen@users.noreply.github.com>
2022-06-28 19:32:23 +0300

    Update SettingsAudio
    SettingsAudio saving now and refreshing it's values

--- Assets/Resources/Scripts/UI/Settings/SettingsAudio.cs
+++ Assets/Resources/Scripts/UI/Settings/SettingsAudio.cs
@@ -2,12 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class SettingsAudio : MonoBehaviour
 {
+    [Header("Mixers")]
     [SerializeField] private AudioMixer _generalMixer;
     [SerializeField] private AudioMixer _gameSoundsMixer;
     [SerializeField] private AudioMixer _ambienceMixer;
+
+    [Header("Sliders")]
+    [SerializeField] private Slider _generalSlider;
+    [SerializeField] private Slider _gameSoundsSlider;
+    [SerializeField] private Slider _ambienceSlider;
 
     public float GeneralVolume { get; private set; }
     public float GameSoundsVolume { get; private set; }
@@ -17,22 +24,43 @@ public class SettingsAudio : MonoBehaviour
     {
         GeneralVolume = volume;
         _generalMixer.SetFloat("Volume", FloatToDecibel(volume));
+        RefreshOptions();
     }
 
     public void SetGameSoundsVolume(float volume)
     {
         GameSoundsVolume = volume;
         _gameSoundsMixer.SetFloat("Volume", FloatToDecibel(volume));
+        RefreshOptions();
     }
 
     public void SetAmbienceVolume(float volume)
     {
         AmbienceVolume = volume;
         _ambienceMixer.SetFloat("Volume", FloatToDecibel(volume));
+        RefreshOptions();
     }
 
     private float FloatToDecibel(float volume)
     {
         return Mathf.Lerp(-80f, 20f, volume);
+    }
+
+    private void RefreshOptions()
+    {
+        _generalSlider.value = GeneralVolume;
+        _gameSoundsSlider.value = GameSoundsVolume;
+        _ambienceSlider.value = AmbienceVolume;
+    }
+
+    private void Start()
+    {
+        SettingsGeneralData data = SettingsSaveSystem.CachedSave;
+
+        GeneralVolume = data.audioData.GeneralVolume;
+        GameSoundsVolume = data.audioData.GameSoundsVolume;
+        AmbienceVolume = data.audioData.AmbienceVolume;
+
+        RefreshOptions();
     }
 }
