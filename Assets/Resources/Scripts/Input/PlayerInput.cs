1b62284ba815c16ecef7e15d995fedce7fb557ec
Eyellen <83500523+Eyellen@users.noreply.github.com>
2022-07-03 00:10:33 +0300

    Update PlayerInput.cs

--- Assets/Resources/Scripts/Input/PlayerInput.cs
+++ Assets/Resources/Scripts/Input/PlayerInput.cs
@@ -7,13 +7,18 @@ public static class PlayerInput
     public static bool IsBlocked { get; set; }
 
     // Bools
-    public static bool IsRightActionPressed { get => IsBlocked ? default(bool) : Input.GetMouseButton(1); }
-    public static bool IsLeftActionPressed { get => IsBlocked ? default(bool) : Input.GetMouseButton(0); }
+    public static bool IsRightActionPressed { get => Condition(Input.GetMouseButton(1)); }
+    public static bool IsLeftActionPressed { get => Condition(Input.GetMouseButton(0)); }
 
     // Floats
-    public static float MouseHorizontalAxis { get => IsBlocked ? default(float) : Input.GetAxis("Mouse X"); }
-    public static float MouseVerticalAxis { get => IsBlocked ? default(float) : Input.GetAxis("Mouse Y"); }
-    public static float HorizontalAxis { get => IsBlocked ? default(float) : Input.GetAxisRaw("Horizontal"); }
-    public static float VerticalAxis { get => IsBlocked ? default(float) : Input.GetAxisRaw("Vertical"); }
-    public static float Brake { get => IsBlocked ? default(float) : Input.GetAxisRaw("Brake"); }
+    public static float MouseHorizontalAxis { get => Condition(Input.GetAxis("Mouse X")); }
+    public static float MouseVerticalAxis { get => Condition(Input.GetAxis("Mouse Y")); }
+    public static float HorizontalAxis { get => Condition(Input.GetAxisRaw("Horizontal")); }
+    public static float VerticalAxis { get => Condition(Input.GetAxisRaw("Vertical")); }
+    public static float Brake { get => Condition(Input.GetAxisRaw("Brake")); }
+
+    private static T Condition<T>(T value)
+    {
+        return IsBlocked ? default(T) : value;
+    }
 }
