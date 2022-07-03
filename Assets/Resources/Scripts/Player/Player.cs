c18d6d31d6e941c2ce829859330fdc8c50093417
Eyellen <83500523+Eyellen@users.noreply.github.com>
2022-07-03 20:55:28 +0300

    Player update
    Now Player is separated prefab with Camera on it

---
+++ Assets/Resources/Scripts/Player/Player.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class Player : NetworkBehaviour
+{
+    public static Player LocalPlayer { get; private set; }
+
+    public Transform CameraTransform { get; private set; }
+
+    [field: SerializeField]
+    [field: SyncVar]
+    public string Username { get; private set; }
+
+    private void Start()
+    {
+        CameraTransform = transform.Find("Camera");
+
+        if (isLocalPlayer)
+        {
+            LocalPlayer = this;
+            CmdSetUsername(Username = SettingsUser.Username);
+            CameraTransform.gameObject.tag = "MainCamera";
+        }
+        else
+        {
+            CameraTransform.gameObject.SetActive(false);
+        }
+    }
+
+    [Command]
+    private void CmdSetUsername(string username)
+    {
+        Username = username;
+    }
+}
