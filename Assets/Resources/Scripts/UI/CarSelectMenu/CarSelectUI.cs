0d550b3fe718e9f67923cccf53fea235f7591c6c
Eyellen <83500523+Eyellen@users.noreply.github.com>
2022-07-04 02:56:23 +0300

    Create CarSelectUI.cs

---
+++ Assets/Resources/Scripts/UI/CarSelectMenu/CarSelectUI.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarSelectUI : MonoBehaviour
+{
+    private void OnEnable()
+    {
+        CursorManager.ShowCursor();
+        PlayerInput.IsBlocked = true;
+    }
+
+    private void OnDisable()
+    {
+        CursorManager.HideCursor();
+        PlayerInput.IsBlocked = false;
+    }
+
+    public void SetActive(bool isActive)
+    {
+        gameObject.SetActive(isActive);
+    }
+
+    public void Confirm()
+    {
+        if (SpawnManager.Instance.SelectedCar == null) return;
+
+        SpawnManager.Instance.CmdSpawn(Player.LocalPlayer.gameObject);
+        SetActive(false);
+    }
+}
