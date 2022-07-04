27ebf2a748083dc916a2b8a50b310e11edb02f82
Eyellen <83500523+Eyellen@users.noreply.github.com>
2022-07-04 16:02:58 +0300

    Update PlayerInput and InputManager
    Added UpDown Axis

--- Assets/Resources/Scripts/Input/PlayerInput.cs
+++ Assets/Resources/Scripts/Input/PlayerInput.cs
@@ -11,10 +11,11 @@ public static class PlayerInput
     public static bool IsLeftActionPressed { get => Condition(Input.GetMouseButton(0)); }
 
     // Floats
-    public static float MouseHorizontalAxis { get => Condition(Input.GetAxis("Mouse X")); }
-    public static float MouseVerticalAxis { get => Condition(Input.GetAxis("Mouse Y")); }
+    public static float MouseHorizontalAxis { get => Condition(Input.GetAxisRaw("Mouse X")); }
+    public static float MouseVerticalAxis { get => Condition(Input.GetAxisRaw("Mouse Y")); }
     public static float HorizontalAxis { get => Condition(Input.GetAxisRaw("Horizontal")); }
     public static float VerticalAxis { get => Condition(Input.GetAxisRaw("Vertical")); }
+    public static float UpDownAxis { get => Condition(Input.GetAxisRaw("UpDown")); }
     public static float Brake { get => Condition(Input.GetAxisRaw("Brake")); }
 
     private static T Condition<T>(T value)
