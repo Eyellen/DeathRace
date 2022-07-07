ec368424b6e04f5d64355dd17ebe159d13645496
Eyellen <83500523+Eyellen@users.noreply.github.com>
2022-07-07 07:06:18 +0300

    Create ServerBrowserUI.cs

---
+++ Assets/Resources/Scripts/UI/ServerBrowser/ServerBrowserUI.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror.Discovery;
+
+public class ServerBrowserUI : MonoBehaviour
+{
+    [SerializeField] private NetworkDiscovery _networkDiscovery;
+
+    public void Connect()
+    {
+        
+    }
+}
