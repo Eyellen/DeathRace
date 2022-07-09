ada4572216476bf5fddc2ad0312f85935bad9f1e
Eyellen <83500523+Eyellen@users.noreply.github.com>
2022-07-10 02:07:29 +0300

    Update ServerDiscoveryUI.cs

--- Assets/Resources/Scripts/UI/ServerBrowser/ServerDiscoveryUI.cs
+++ Assets/Resources/Scripts/UI/ServerBrowser/ServerDiscoveryUI.cs
@@ -10,6 +10,7 @@ public class ServerDiscoveryUI : MonoBehaviour
     [SerializeField] private GameObject _serversListArea;
     [SerializeField] private GameObject _serverInfoBarTemplatePrefab;
 
+#if UNITY_EDITOR
     private void OnValidate()
     {
         if(_networkDiscovery == null)
@@ -19,6 +20,7 @@ public class ServerDiscoveryUI : MonoBehaviour
             UnityEditor.Undo.RecordObjects(new Object[] { this, _networkDiscovery }, "Set NetworkDiscovery");
         }
     }
+#endif
 
     public void FindServers()
     {
@@ -28,11 +30,17 @@ public class ServerDiscoveryUI : MonoBehaviour
 
     public void OnDiscoveredServer(DiscoveryResponse info)
     {
-        Instantiate(_serverInfoBarTemplatePrefab, _serversListArea.transform);
         Debug.Log(info.ServerName);
         Debug.Log(info.MaxPlayersCount);
         Debug.Log(info.CurrentPlayersCount);
         Debug.Log(info.MaxPing);
+
+        var serverInfoBar = Instantiate(_serverInfoBarTemplatePrefab, _serversListArea.transform).GetComponent<ServerInfoBarUI>();
+
+        serverInfoBar.ServerName = info.ServerName;
+        serverInfoBar.PlayersCount = $"{info.CurrentPlayersCount}/{info.MaxPlayersCount}";
+        serverInfoBar.MaxPing = info.MaxPing.ToString();
+        //serverInfoBar.Region = info.Region;
     }
 
     private IEnumerator ClearServerListCoroutine()
