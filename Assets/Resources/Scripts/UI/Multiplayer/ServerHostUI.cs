85d96abf88a2c4a0832dd168b50c3084cbad11d9
Eyellen <83500523+Eyellen@users.noreply.github.com>
2022-07-13 19:09:46 +0300

    Renamed ServerManagerUI.cs to ServerHost.cs

--- Assets/Resources/Scripts/UI/Multiplayer/ServerHostUI.cs
+++ Assets/Resources/Scripts/UI/Multiplayer/ServerHostUI.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using Mirror;
 
-public class ServerManagerUI : MonoBehaviour
+public class ServerHostUI : MonoBehaviour
 {
     private NetworkManager _networkManager;
     private NewNetworkDiscovery _networkDiscovery;
