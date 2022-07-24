f8c28e7e2e5c85ea9f4cd2e7ff62798968f23d58
Eyellen <83500523+Eyellen@users.noreply.github.com>
2022-07-24 21:36:25 +0300

    Update RaceModeManager.cs

--- Assets/Resources/Scripts/GameModes/RaceMode/RaceModeManager.cs
+++ Assets/Resources/Scripts/GameModes/RaceMode/RaceModeManager.cs
@@ -9,6 +9,8 @@ public class RaceModeManager : MonoBehaviour
     [field: SerializeField] 
     public CheckPoint[] CheckPoints { get; private set; }
 
+    private int _lapsCompleted = 0;
+
     private void Start()
     {
         InitializeInstance();
@@ -27,7 +29,47 @@ public class RaceModeManager : MonoBehaviour
     {
         for (int i = 0; i < CheckPoints.Length; i++)
         {
+            // Initializing index
             CheckPoints[i].CheckPointIndex = i;
+
+            // Initializing event
+            CheckPoints[i].OnCheckPointPassed += OnCheckPointPassed;
+        }
+    }
+
+    private void OnCheckPointPassed(int checkPointIndex)
+    {
+        // When passing last point we are resetting initial point to be able to pass it
+        if (checkPointIndex == CheckPoints.Length - 1)
+        {
+            CheckPoints[0].ResetPoint();
+        }
+
+        if (checkPointIndex == 0)
+        {
+            if(CheckPoints[CheckPoints.Length - 1].IsPassed)
+            {
+                _lapsCompleted++;
+                ResetAllCheckPoints();
+            }
+
+            CheckPoints[checkPointIndex].MarkAsPassed();
+
+            return;
+        }
+
+        // Mark current point as passed only if previous point was passed
+        if (CheckPoints[checkPointIndex - 1].IsPassed)
+        {
+            CheckPoints[checkPointIndex].MarkAsPassed();
+        }
+    }
+
+    private void ResetAllCheckPoints()
+    {
+        foreach (var checkPoint in CheckPoints)
+        {
+            checkPoint.ResetPoint();
         }
     }
 }
