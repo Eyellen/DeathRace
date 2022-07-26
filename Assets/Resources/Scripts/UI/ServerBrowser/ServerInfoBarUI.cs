956e3f25e4d449ef5c08c04e3752316c7b13c064
Eyellen <83500523+Eyellen@users.noreply.github.com>
2022-07-26 22:28:56 +0300

    Update ServerInfoBarUI.cs
    Added GameModeIndex field
    Added SetId method

--- Assets/Resources/Scripts/UI/ServerBrowser/ServerInfoBarUI.cs
+++ Assets/Resources/Scripts/UI/ServerBrowser/ServerInfoBarUI.cs
@@ -8,14 +8,26 @@ public class ServerInfoBarUI : MonoBehaviour
 {
     public ServerDiscoveryUI ServerDiscoveryUI { get; set; }
 
+    [SerializeField] private TextMeshProUGUI _gameModeText;
     [SerializeField] private TextMeshProUGUI _serverNameText;
     [SerializeField] private TextMeshProUGUI _playersCountText;
     [SerializeField] private TextMeshProUGUI _maxPingText;
     [SerializeField] private TextMeshProUGUI _RegionText;
 
+    private int _gameModeIndex;
+
     public long ServerId { get; set; }
     public Uri Uri { get; set; }
 
+    public int GameModeIndex
+    {
+        get => _gameModeIndex;
+        set
+        {
+            _gameModeIndex = value;
+            _gameModeText.text = ((GameMode)_gameModeIndex).ToString();
+        }
+    }
     public string ServerName { get { return _serverNameText.text; } set { _serverNameText.text = value; } }
     public string PlayersCount { get { return _playersCountText.text; } set { _playersCountText.text = value; } }
     public string MaxPing { get { return _maxPingText.text; } set { _maxPingText.text = value; } }
@@ -24,5 +36,11 @@ public class ServerInfoBarUI : MonoBehaviour
     public void SetAddress()
     {
         ServerDiscoveryUI.SelectedServerUri = Uri;
+        SetId();
     }
+
+    private void SetId()
+    {
+        ServerDiscoveryUI.SelectedServerId = ServerId;
+    }    
 }
