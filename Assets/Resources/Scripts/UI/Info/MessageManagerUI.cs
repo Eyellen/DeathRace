24e6f56cd6c871b671d32b190173aecfa0231f61
Eyellen <83500523+Eyellen@users.noreply.github.com>
2022-07-29 21:19:34 +0300

    Create MessageManagerUI.cs

---
+++ Assets/Resources/Scripts/UI/Info/MessageManagerUI.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class MessageManagerUI : MonoBehaviour
+{
+    public static MessageManagerUI Instance { get; private set; }
+
+    [SerializeField] private TextMeshProUGUI _bottonMessageBar;
+
+    private void Start()
+    {
+        InitializeInstance();
+    }
+
+    private void InitializeInstance()
+    {
+        if (Instance == null)
+            Instance = this;
+        else
+        {
+#if UNITY_EDITOR
+            Debug.Log($"Trying to create duplicate of {nameof(MessageManagerUI)} when it's not allowed. " +
+                $"The duplicate will be destroyed.");
+#endif
+            Destroy(gameObject);
+        }
+    }
+
+    private void ShowBottonMessage(string msg)
+    {
+        _bottonMessageBar.text = msg;
+        _bottonMessageBar.gameObject.SetActive(true);
+    }
+
+    private void HideBottonMessage()
+    {
+        _bottonMessageBar.gameObject.SetActive(false);
+    }
+}
