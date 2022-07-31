3ace375f79fb772ae26cabcf889cfe19e9d3bdc2
Eyellen <83500523+Eyellen@users.noreply.github.com>
2022-07-31 23:43:42 +0300

    Update GameModeBase.cs

--- Assets/Resources/Scripts/GameModes/GameModeBase.cs
+++ Assets/Resources/Scripts/GameModes/GameModeBase.cs
@@ -10,10 +10,13 @@ public class GameModeBase : NetworkBehaviour
     private bool _isInitialized = false;
 
     [field: SyncVar]
-    public bool IsWaitingForPlayers { get; protected set; }
+    public bool IsWaitingForPlayers { get; private set; }
 
     [field: SyncVar]
-    public bool IsGameStarted { get; protected set; }
+    public bool IsGameStarting { get; private set; }
+
+    [field: SyncVar]
+    public bool IsGameOn { get; private set; }
 
     private void InitializeInstance()
     {
@@ -48,7 +51,55 @@ public class GameModeBase : NetworkBehaviour
         gameObject.SetActive(false);
     }
 
-    protected virtual void StartGame() { }
+    protected virtual void Update()
+    {
+        ServerUpdate();
+    }
+    
+    [ServerCallback]
+    protected virtual void ServerUpdate()
+    {
+        // Starting game by button
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            StartCoroutine(StartGameCoroutine(3));
+        }
+    }
+
+    [Server]
+    protected virtual void StartGame() 
+    {
+        if (IsGameOn) return;
+        IsGameStarting = false;
+        IsGameOn = true;
+    }
+
+    [Server]
     protected virtual void StopGame() { }
+
+    [Server]
     protected virtual void RestartGame() { }
+
+    /// <summary>
+    /// Server method.
+    /// Starts game in seconds via StartGame() method and displays message to all clients
+    /// </summary>
+    /// <param name="seconds"></param>
+    /// <returns></returns>
+    [Server]
+    protected IEnumerator StartGameCoroutine(int seconds)
+    {
+        if (IsGameStarting) yield break;
+        IsGameStarting = true;
+
+        while (seconds > 0)
+        {
+            MessageManager.Instance.RpcShowBottomMessage($"The game is starting in {seconds} seconds");
+            yield return new WaitForSeconds(1);
+            seconds--;
+        }
+        MessageManager.Instance.RpcHideBottomMessage();
+
+        StartGame();
+    }
 }
