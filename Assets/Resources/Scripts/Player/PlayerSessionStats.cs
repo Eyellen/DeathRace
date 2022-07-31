643329be855478cc88680200a5509270abd28223
Eyellen <83500523+Eyellen@users.noreply.github.com>
2022-07-31 06:56:21 +0300

    Create PlayerSessionStats.cs

---
+++ Assets/Resources/Scripts/Player/PlayerSessionStats.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class PlayerSessionStats : NetworkBehaviour
+{
+    private void Start()
+    {
+        //NetworkClient.localPlayer.netId;
+
+        //NetworkServer.connections
+
+        //NetworkClient.connection.connectionId
+
+        //Player.LocalPlayer.Car.GetComponent<CarBase>().netId
+    }
+}
