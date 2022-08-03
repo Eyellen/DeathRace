d037b9df6123b430799415d4941a5ffb1ac2d7ba
Eyellen <83500523+Eyellen@users.noreply.github.com>
2022-08-03 16:33:39 +0300

    Update EscapeMenu.cs

--- Assets/Resources/Scripts/UI/EscapeMenu.cs
+++ Assets/Resources/Scripts/UI/EscapeMenu.cs
@@ -1,10 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using Mirror;
 
 public class EscapeMenu : MonoBehaviour
 {
     [SerializeField] private GameObject _settingsMenu;
+
+    [SerializeField] private Button _restartButton;
+
+    private void Start()
+    {
+        if ((GameMode)ServerData.GameModeIndex != GameMode.Free)
+            GameModeBase.OnInitialized += InitializeRestartButton;
+
+        gameObject.SetActive(false);
+    }
+
+    private void Update()
+    {
+        PlayerInput.IsBlocked = true;
+        CursorManager.ShowCursor();
+    }
 
     private void OnEnable()
     {
@@ -27,5 +45,15 @@ public class EscapeMenu : MonoBehaviour
     public void SetActive(bool isActive)
     {
         gameObject.SetActive(isActive);
+    }
+
+    private void InitializeRestartButton()
+    {
+        if(NetworkServer.active)
+        {
+            _restartButton.onClick.AddListener(() => GameModeBase.Instance.RestartGame());
+            GameModeBase.Instance.OnGameStarted += () => _restartButton.interactable = true;
+            GameModeBase.Instance.OnGameEnded += () => _restartButton.interactable = false;
+        }
     }
 }
