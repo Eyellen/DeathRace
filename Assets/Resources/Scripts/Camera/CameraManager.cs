3ed8abb13d674c9380f22b73a70c55e58101b2fc
Eyellen <83500523+Eyellen@users.noreply.github.com>
2022-08-07 04:34:33 +0300

    Update CameraManager.cs
    Fixed bug when after current car has been destroyed and spawned new one in 2 seconds (chasing car time after destroying) camera was switching to free though car was still active

--- Assets/Resources/Scripts/Camera/CameraManager.cs
+++ Assets/Resources/Scripts/Camera/CameraManager.cs
@@ -13,6 +13,8 @@ public class CameraManager : MonoBehaviour
 
     public CameraBase CurrentCameraScript { get => _currentCameraScript; }
 
+    private IEnumerator _setFreeCameraCoroutine;
+
     public CameraMode CameraMode
     {
         get => _cameraMode;
@@ -87,7 +89,6 @@ public class CameraManager : MonoBehaviour
     {
         CameraMode = CameraMode.ThirdPerson;
         ((ThirdPersonCamera)_currentCameraScript).Target = target;
-        //target.gameObject.GetComponent<CarDamageable>().OnCarDestroyed += SetFreeCamera;
     }
 
     public void SetFreeCamera()
@@ -97,11 +98,15 @@ public class CameraManager : MonoBehaviour
 
     public void SetFreeCamera(float seconds)
     {
-        StartCoroutine(SetFreeCameraCoroutine(seconds));
+        _setFreeCameraCoroutine = SetFreeCameraCoroutine(seconds);
+        StartCoroutine(_setFreeCameraCoroutine);
     }
 
     private IEnumerator SetFreeCameraCoroutine(float seconds)
     {
+        if (_setFreeCameraCoroutine != null)
+            StopCoroutine(_setFreeCameraCoroutine);
+
         yield return new WaitForSeconds(seconds);
         CameraMode = CameraMode.Free;
     }
