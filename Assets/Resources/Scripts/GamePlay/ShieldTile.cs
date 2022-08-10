0cdf5352523fc07c0cf94424cad2830af760b69e
Eyellen <83500523+Eyellen@users.noreply.github.com>
2022-08-10 22:55:43 +0300

    Added ShieldTile.cs

---
+++ Assets/Resources/Scripts/GamePlay/ShieldTile.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShieldTile : TileBase
+{
+    protected override void OnCarEnter(GameObject car)
+    {
+        throw new System.NotImplementedException();
+    }
+}
