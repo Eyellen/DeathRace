07ea54bd7c4d387805f27563b57d6fc20a1f6a5e
Eyellen <83500523+Eyellen@users.noreply.github.com>
2022-08-11 21:32:29 +0300

    Create Spikes.cs

---
+++ Assets/Resources/Scripts/GamePlay/Spikes.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spikes : MonoBehaviour
+{
+    [SerializeField] private float _minSpeedForDamage;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (!collision.gameObject.TryGetComponent(out IDamageable<int> damageable)) return;
+
+        Debug.Log(collision.relativeVelocity.magnitude);
+
+        if (collision.relativeVelocity.magnitude < _minSpeedForDamage) return;
+
+        float coefficient = (collision.relativeVelocity.magnitude - _minSpeedForDamage) / (25 - _minSpeedForDamage);
+        damageable.Damage01(coefficient, collision.collider);
+    }
+}
