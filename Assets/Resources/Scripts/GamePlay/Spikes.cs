9feefb0aa4cb60677154e3203255b1625862990a
Eyellen <83500523+Eyellen@users.noreply.github.com>
2022-08-12 02:48:13 +0300

    Update Spikes.cs

--- Assets/Resources/Scripts/GamePlay/Spikes.cs
+++ Assets/Resources/Scripts/GamePlay/Spikes.cs
@@ -8,13 +8,24 @@ public class Spikes : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (!collision.gameObject.TryGetComponent(out IDamageable<int> damageable)) return;
-
-        Debug.Log(collision.relativeVelocity.magnitude);
-
         if (Mathf.Abs(collision.relativeVelocity.magnitude) < _minSpeedForDamage) return;
 
+        IDamageable<int>[] damageables = collision.gameObject.GetComponents<IDamageable<int>>();
+        if (damageables.Length == 0) return;
+
         float coefficient = (Mathf.Abs(collision.relativeVelocity.magnitude) - _minSpeedForDamage) / (25 - _minSpeedForDamage);
-        damageable.Damage01(coefficient, collision.collider);
+        foreach (var damageable in damageables)
+        {
+            damageable.Damage01(coefficient, collision.collider);
+        }
+
+        //if (!collision.gameObject.TryGetComponent(out IDamageable<int> damageable)) return;
+
+        //Debug.Log(collision.relativeVelocity.magnitude);
+
+        //if (Mathf.Abs(collision.relativeVelocity.magnitude) < _minSpeedForDamage) return;
+
+        //float coefficient = (Mathf.Abs(collision.relativeVelocity.magnitude) - _minSpeedForDamage) / (25 - _minSpeedForDamage);
+        //damageable.Damage01(coefficient, collision.collider);
     }
 }
