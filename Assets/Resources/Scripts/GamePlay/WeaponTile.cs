51a6445b1fb9f386b0f25da8206cba531368a5c5
Eyellen <83500523+Eyellen@users.noreply.github.com>
2022-08-11 18:15:31 +0300

    Update WeaponTile.cs
    Adapted WeaponTile.cs for multiplayer

--- Assets/Resources/Scripts/GamePlay/WeaponTile.cs
+++ Assets/Resources/Scripts/GamePlay/WeaponTile.cs
@@ -1,19 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Mirror;
 
 public class WeaponTile : TileBase
 {
     protected override void OnCarEnter(GameObject car)
     {
-        if(car.TryGetComponent(out GunBase gunBase))
+        CmdActivateWeapons(car);
+    }
+
+    [Command(requiresAuthority = false)]
+    private void CmdActivateWeapons(GameObject car)
+    {
+        if (car.TryGetComponent(out GunBase gunBase))
         {
             GunBase[] guns = car.GetComponents<GunBase>();
             foreach (var gun in guns)
                 gun.IsActivated = true;
         }
-        
-        if(car.TryGetComponent(out RocketLauncher rocketLauncher))
+
+        if (car.TryGetComponent(out RocketLauncher rocketLauncher))
         {
             rocketLauncher.IsActivated = true;
         }
