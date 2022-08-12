871edff67ae37c3cdea6df1c2f1ca711174d1424
Eyellen <83500523+Eyellen@users.noreply.github.com>
2022-08-12 17:49:49 +0300

    Update GameCanvas.cs
    Added ability to hide HUD by pressing F1

--- Assets/Resources/Scripts/UI/GameCanvas.cs
+++ Assets/Resources/Scripts/UI/GameCanvas.cs
@@ -7,6 +7,8 @@ public class GameCanvas : MonoBehaviour
     [SerializeField] private EscapeMenu _escapeMenu;
     [SerializeField] private CarSelectUI _carSelectMenu;
 
+    [SerializeField] private GameObject[] _objectsToHide;
+
     private void Start()
     {
         //CursorManager.HideCursor();
@@ -33,6 +35,14 @@ public class GameCanvas : MonoBehaviour
         if(Input.GetKeyDown(KeyCode.N))
         {
             _carSelectMenu.SetActive(!_carSelectMenu.gameObject.activeSelf);
+        }
+
+        if(Input.GetKeyDown(KeyCode.F1))
+        {
+            foreach (var objectToHide in _objectsToHide)
+            {
+                objectToHide.SetActive(!objectToHide.activeSelf);
+            }
         }
     }
 }
