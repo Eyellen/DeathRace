6b3fbc82db250825b93974e8b9f9ed2c433f29fb
Eyellen <83500523+Eyellen@users.noreply.github.com>
2022-08-18 05:04:43 +0300

    Create Credits.cs

---
+++ Assets/Resources/Scripts/UI/CreditsUI.cs
@@ -0,0 +1,27 @@
+using System.Diagnostics;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreditsUI : MonoBehaviour
+{
+    public void HdtLink()
+    {
+        Process.Start("https://github.com/Happiness-Dream-Team");
+    }
+
+    public void EyellenLink()
+    {
+        Process.Start("https://github.com/Eyellen");
+    }
+
+    public void NeoTyrantLink()
+    {
+        Process.Start("https://github.com/NeoTyrant");
+    }
+
+    public void ProjectLink()
+    {
+        Process.Start("https://github.com/Eyellen/DeathRace/tree/main");
+    }
+}
