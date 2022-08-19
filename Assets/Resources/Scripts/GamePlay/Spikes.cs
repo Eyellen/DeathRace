58576b40f82a46c7530e79bd2918f8617c5aa530
Eyellen <83500523+Eyellen@users.noreply.github.com>
2022-08-19 05:51:30 +0300

    Update Spikes.cs

--- Assets/Resources/Scripts/GamePlay/Spikes.cs
+++ Assets/Resources/Scripts/GamePlay/Spikes.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Spikes : MonoBehaviour
 {
     [SerializeField] private float _minSpeedForDamage;
+    [SerializeField] private float _damageCoefficientMultiplier = 1;
 
     private void OnCollisionEnter(Collision collision)
     {
@@ -22,7 +23,7 @@ public class Spikes : MonoBehaviour
         IDamageable<int>[] damageables = collision.gameObject.GetComponents<IDamageable<int>>();
         if (damageables.Length == 0) return;
 
-        float coefficient = (Mathf.Abs(collision.relativeVelocity.magnitude) - _minSpeedForDamage) / (25 - _minSpeedForDamage);
+        float coefficient = (Mathf.Abs(collision.relativeVelocity.magnitude) - _minSpeedForDamage) / (25 - _minSpeedForDamage) * _damageCoefficientMultiplier;
         foreach (var damageable in damageables)
         {
             damageable.Damage01(coefficient, collision.collider);
