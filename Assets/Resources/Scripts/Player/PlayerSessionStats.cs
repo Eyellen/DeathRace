c3d47c82773ed98606a6cfde41889f5ebab18ead
Eyellen <83500523+Eyellen@users.noreply.github.com>
2022-08-22 06:38:12 +0300

    Update PlayerSessionStats.cs

--- Assets/Resources/Scripts/Player/PlayerSessionStats.cs
+++ Assets/Resources/Scripts/Player/PlayerSessionStats.cs
@@ -5,5 +5,19 @@ using Mirror;
 
 public class PlayerSessionStats : NetworkBehaviour
 {
+    [field: SyncVar]
+    public int Kills { get; set; }
 
+    [field: SyncVar]
+    public int LapsCompleted { get; set; }
+
+    public void CmdSetKills(int kills)
+    {
+        Kills = kills;
+    }
+
+    public void CmdSetLapsCompleted(int lapsCompleted)
+    {
+        LapsCompleted = lapsCompleted;
+    }
 }
