3fa7d867e2e8b569f81aa658ea3dc60163f75e59
Eyellen <83500523+Eyellen@users.noreply.github.com>
2022-08-23 06:01:18 +0300

    Upgraded PlayerListUI
    Now it displays player's status

--- Assets/Resources/Scripts/UI/PlayerList/PlayerInfoBarUI.cs
+++ Assets/Resources/Scripts/UI/PlayerList/PlayerInfoBarUI.cs
@@ -6,6 +6,10 @@ using TMPro;
 
 public class PlayerInfoBarUI : MonoBehaviour
 {
+    [SerializeField] private Color _spectatingColor = new Color32(212, 212, 212, 255);
+    [SerializeField] private Color _aliveColor = new Color32(21, 255, 0, 255);
+    [SerializeField] private Color _eliminatedColor = new Color32(255, 0, 0, 255);
+
     private Player _player;
     public Player Player
     {
@@ -24,7 +28,26 @@ public class PlayerInfoBarUI : MonoBehaviour
     [SerializeField] private TextMeshProUGUI _killsText;
 
     public string Username { get => _usernameText.text; private set => _usernameText.text = value; }
-    public string PlayerStatus { get => Enum.Parse(typeof(), _playerStatus.text); }
+    public PlayerStatus PlayerStatus 
+    { 
+        get => (PlayerStatus)Enum.Parse(typeof(PlayerStatus), _playerStatus.text);
+        set
+        {
+            _playerStatus.text = value.ToString();
+            switch (value)
+            {
+                case PlayerStatus.Spectator:
+                    _playerStatus.color = _spectatingColor;
+                    break;
+                case PlayerStatus.Alive:
+                    _playerStatus.color = _aliveColor;
+                    break;
+                case PlayerStatus.Eliminated:
+                    _playerStatus.color = _eliminatedColor;
+                    break;
+            }
+        }
+    }
     public int LapsCompleted { get => int.Parse(_usernameText.text); private set => _lapsText.text = value.ToString(); }
     public int Kills { get => int.Parse(_killsText.text); private set => _killsText.text = value.ToString(); }
 
@@ -43,6 +66,8 @@ public class PlayerInfoBarUI : MonoBehaviour
 
     private void OnEnable()
     {
+        UpdateAllFields();
+
         if (_updateCoroutine != null)
             StopCoroutine(_updateCoroutine);
         _updateCoroutine = UpdateCoroutine();
@@ -70,6 +95,7 @@ public class PlayerInfoBarUI : MonoBehaviour
     private void UpdateAllFields()
     {
         Username = _player.Username;
+        PlayerStatus = _player.SessionStats.PlayerStatus;
         LapsCompleted = _player.SessionStats.LapsCompleted;
         Kills = _player.SessionStats.Kills;
     }
