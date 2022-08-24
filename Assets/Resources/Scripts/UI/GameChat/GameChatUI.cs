92d1186bddb13a7163ad05bbf0446b695bb31580
Eyellen <83500523+Eyellen@users.noreply.github.com>
2022-08-24 16:51:38 +0300

    Create GameChatUI.cs

---
+++ Assets/Resources/Scripts/UI/GameChat/GameChatUI.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameChatUI : MonoBehaviour
+{
+    [SerializeField] private GameObject _chatContent;
+    [SerializeField] private GameObject _chatMessagePrefab;
+
+
+}
